5241b99973b86ac1f76cb140cab7d8903b193ce1
armen12 <armennagapetyan1996@gmail.com>
2018-01-15 14:12:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ load_dotenv(find_dotenv())
 @app.route('/')
 def token():
     # Get credentials for environment variables
-    account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    api_key = os.environ['TWILIO_API_KEY']
-    api_secret = os.environ['TWILIO_API_SECRET']
+    account_sid = 'ACbce0865b277c75152da6fc469b650463'
+    api_key = 'SK2e4b45c9cb8835c6ad1a614c3346b209'
+    api_secret = 'l6GKan4KkOKCmiktZDTmhahRY7q2aTPz'
 
     # Create an Access Token
     token = AccessToken(account_sid, api_key, api_secret)
